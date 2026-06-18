d85a00fc6ddb7f4942489f150020649e573b6c9a
Harsh <iharshgaur997@gmail.com>
2026-06-18 09:57:56 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const prefix = new Array(n);
+        prefix[0] = 1;
+        for (let i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        let suffix = 1;
+        let sol = new Array(n)
+        for (let i = n - 1; i >= 0; i--) {
+            sol[i] = prefix[i] * suffix;
+            suffix *= nums[i];
+
+        }
+        return sol;
+    }}
